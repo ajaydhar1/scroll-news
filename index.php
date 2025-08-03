88e90b211c857935095975fff2b7e8faff3940a2
Ajay Dhar <ajaydhar108@gmail.com>
2025-08-03 02:14:46 -0400

    Increased line-height in introjs

--- index.php
+++ index.php
@@ -320,7 +320,7 @@ foreach ($og as $key => $value) {
 
             .introjs-tooltiptext {
                 font-size: 18px;
-                line-height: calc(var(--line-height-unit) * 1.17) !important;
+                line-height: calc(var(--line-height-unit) * 1.22) !important;
             }
 
             .analyze-btn {
