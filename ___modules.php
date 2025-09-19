fa12f90d191eeaf47957718e6f298ac7ce298cd2
Ajay Dhar <ajaydhar108@gmail.com>
2025-09-18 21:27:32 -0400

    Fixed bug in getNLPFromDB() function

--- ___modules.php
+++ ___modules.php
@@ -517,7 +517,7 @@ function getRandomArticle_fromDB() {
 }
 
 // Returns the row or null if not found
-function getNLPFromDB(PDO $pdo, string $url): ?array {
+function getNLPFromDB(string $url): ?array {
     
     $pdo = getPdoOrExplain();
     if (!$pdo) {
