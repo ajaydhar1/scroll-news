c13e24f00f9e4858aaaebbc69645f1ef09ad5dcd
Ajay Dhar <ajaydhar108@gmail.com>
2025-10-05 02:46:52 -0400

    Blocking random articles with no emotional reaction

--- ___modules.php
+++ ___modules.php
@@ -566,7 +566,7 @@ function getRandomArticle_fromDB(bool $requireEntities = true): array {
     $entitiesClause = '';
     if ($requireEntities) {
       // Requires: ..."entities": [ <something not just ] >
-      $entitiesClause = " AND (nlp::text) NOT LIKE '%\"entities\": []%' AND (nlp::text) NOT LIKE '%\"entities\": [{\"text\": \"X-Forbidden\", \"count\": 1, \"label\": \"ORG\"}]%'";
+      $entitiesClause = " AND (nlp::text) NOT LIKE '%\"entities\": []%' AND (nlp::text) NOT LIKE '%\"entities\": [{\"text\": \"X-Forbidden\", \"count\": 1, \"label\": \"ORG\"}]%' AND (nlp::text) NOT LIKE '%\"emotional_reaction\": {}%'";
     }
 
     // Base ready predicate (no JSON casts)
