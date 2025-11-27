00624fdeab7fb0ad0bdcdc88c5330e3180dea742
Ajay Dhar <ajaydhar108@gmail.com>
2025-11-27 16:52:41 -0500

    Add 404 and 500 error pages

--- index.php
+++ index.php
@@ -30,8 +30,6 @@
     
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-
-
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         <link href="css/custom.css" rel="stylesheet" />
