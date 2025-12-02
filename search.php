759db2d7a8b747ddaaf44a4dc9f4c758182321a5
Ajay Dhar <ajaydhar108@gmail.com>
2025-12-01 23:31:29 -0500

    Update links to style and custom css files

--- search.php
+++ search.php
@@ -85,8 +85,9 @@ function sn_format_pub_date(?string $raw): string {
     <title>Search • Scroll News</title>
 
     <!-- You can reuse whatever CSS/JS includes you use on the newsroom page -->
-    <link rel="stylesheet" href="/assets/bootstrap.min.css" />
-    <link rel="stylesheet" href="/assets/site.css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/custom.css" rel="stylesheet" />
 </head>
 <body class="bg-light">
 
