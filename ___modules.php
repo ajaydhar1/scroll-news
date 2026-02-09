ea5710df55355710a35fffa4f29ba90f9e0d9327
Ajay Dhar <ajaydhar108@gmail.com>
2026-02-08 23:30:14 -0500

    Increment cache version

--- ___modules.php
+++ ___modules.php
@@ -2069,7 +2069,7 @@ function _fragment_cache_path(string $key): string {
     return _fragment_cache_dir() . '/' . $safe . '.html';
 }
 
-$CACHE_VER = 'v9';
+$CACHE_VER = 'v10';
 
 function fragment_cache_swr(
     string $key,
