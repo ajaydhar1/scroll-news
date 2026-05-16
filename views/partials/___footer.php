85fa9e17e075a1713a8a31e94d9e64b8ac256853
Ajay Dhar <ajaydhar108@gmail.com>
2026-05-15 20:23:11 -0400

    Update footer social links for new @scrollnewsai branding

--- views/partials/___footer.php
+++ views/partials/___footer.php
@@ -4,11 +4,11 @@
             <div class="row align-items-center">
                 <div class="col-lg-4 text-lg-left">Copyright © Scroll News <?= date('Y') ?></div>
                 <div class="col-lg-4 my-3 my-lg-0">
-                    <a class="btn btn-black btn-social mx-2" title="X profile" href="https://x.com/scrollnewsio" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a class="btn btn-black btn-social mx-2" title="X profile" href="https://x.com/scrollnewsai" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
                     <a class="btn btn-black btn-social mx-2" title="Scroll Archive" href="scroll-archive.php" data-loading><i class="fas fa-history"></i></a>
                     <a class="btn btn-green btn-social mx-2" title="Stumble through articles" href="newsroom.php" onclick="trackStumbleClick('footer')" data-loading><i class="fas fa-play"></i></a>
                     <a class="btn btn-black btn-social mx-2" title="Control Room" href="control-room.php"><i class="fas fa-dashboard"></i></a>
-                    <a class="btn btn-black btn-social mx-2" title="IG profile" href="https://www.instagram.com/scrollnewsio/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    <a class="btn btn-black btn-social mx-2" title="IG profile" href="https://www.instagram.com/scrollnewsai/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
                 </div>
                 <div class="col-lg-4 text-lg-right font-weight-bold">
                     <a href="/" data-loading>scroll news</a>
