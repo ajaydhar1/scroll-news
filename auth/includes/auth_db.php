25e99100e9baf77c1486d6d5ef090723c031112e
Ajay Dhar <ajaydhar108@gmail.com>
2026-05-22 19:10:18 -0400

    Fix mobile Browse News button text wrapping

--- auth/includes/auth_db.php
+++ auth/includes/auth_db.php
@@ -32,8 +32,9 @@ function auth_db(): PDO
     |
     */
 
-    $databaseUrl = getenv('DATABASE_URL');
-    
+    //$databaseUrl = getenv('DATABASE_URL');
+    $databaseUrl = "postgres://u54p8tqv3cg377:p07d3f3181a94264cd3a103e335f8fa769dccd2ca4b9788a78cd5f660fcbfd1e1@c12662383iu6b3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/daa88slg44bj7f";
+
     if (!$databaseUrl) {
 
         error_log('DATABASE_URL environment variable is missing.');
