c7ed746fa795aad15469660b4123345815f03e3e
Ajay Dhar <ajaydhar108@gmail.com>
2026-05-26 21:16:45 -0400

    Convert account dropdown into responsive mega menu navigation

--- views/partials/___account_menu.php
+++ views/partials/___account_menu.php
@@ -37,7 +37,7 @@ $initial = strtoupper(substr($firstName ?: $email ?: 'U', 0, 1));
 
     </a>
 
-    <div class="dropdown-menu dropdown-menu-right account-nav-menu" aria-labelledby="accountDropdown">
+    <div class="dropdown-menu dropdown-menu-right account-nav-menu account-mega-menu" aria-labelledby="accountDropdown">
 
         <div class="dropdown-header account-dropdown-header">
             <div class="font-weight-bold">
@@ -53,13 +53,57 @@ $initial = strtoupper(substr($firstName ?: $email ?: 'U', 0, 1));
 
         <div class="dropdown-divider"></div>
 
-        <a class="dropdown-item" href="/account/">
-            <i class="fa-solid fa-user mr-2"></i> Account
-        </a>
+        <div class="account-mega-grid">
 
-        <a class="dropdown-item" href="/auth/change-password.php">
-            <i class="fa-solid fa-lock mr-2"></i> Change Password
-        </a>
+            <a class="account-mega-link" href="/account/">
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>
+                    <strong>Dashboard</strong>
+                    <small>Account overview</small>
+                </span>
+            </a>
+
+            <a class="account-mega-link" href="/account/reading-history.php" data-loading>
+                <i class="fa-solid fa-book-open-reader"></i>
+                <span>
+                    <strong>Reading History</strong>
+                    <small>Articles you opened</small>
+                </span>
+            </a>
+
+            <a class="account-mega-link" href="/account/saved-headlines.php" data-loading>
+                <i class="fa-regular fa-bookmark"></i>
+                <span>
+                    <strong>Saved Headlines</strong>
+                    <small>Your bookmarked stories</small>
+                </span>
+            </a>
+
+            <a class="account-mega-link" href="/account/search-history.php" data-loading>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <span>
+                    <strong>Search History</strong>
+                    <small>Past news searches</small>
+                </span>
+            </a>
+
+            <a class="account-mega-link" href="/account/shuffle-history.php" data-loading>
+                <i class="fa-solid fa-shuffle"></i>
+                <span>
+                    <strong>Shuffle History</strong>
+                    <small>Saved discovery sessions</small>
+                </span>
+            </a>
+
+            <a class="account-mega-link" href="/auth/change-password.php">
+                <i class="fa-solid fa-lock"></i>
+                <span>
+                    <strong>Password</strong>
+                    <small>Change login password</small>
+                </span>
+            </a>
+
+        </div>
 
         <div class="dropdown-divider"></div>
 
