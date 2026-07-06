8a95a0ef18b5a046897ee4132ff157835f5d6899
Ajay Dhar <ajaydhar108@gmail.com>
2026-07-06 15:43:31 -0400

    Add local configuration example file

---
+++ core/config/local.example.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Local Configuration
+|--------------------------------------------------------------------------
+|
+| Copy this file to:
+|
+|     local.php
+|
+| Then replace the placeholder values with your own local credentials.
+| This file is committed to Git.
+| local.php is ignored by Git.
+|
+*/
+
+define(
+    'DATABASE_URL',
+    'postgres://username:password@host:5432/database'
+);
